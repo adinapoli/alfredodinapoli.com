320adc4a1392f6c4e4610ac7ffbb38b542029f1d
Alfredo Di Napoli <alfredodinapoli@MacchyPro.local>
2011-11-12 19:35:06 +0100

    Now I can put inside the content folder every html of a not-well-specified category

--- site.hs
+++ site.hs
@@ -1,34 +1,44 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Control.Arrow ((>>>), arr)
+import Control.Arrow ((>>>), arr, (>>^))
 
 import Hakyll
+import Hakyll.Core.Writable.CopyFile (CopyFile)
 
 staticPageCompiler = readPageCompiler >>> 
                      addDefaultFields >>>
                      arr applySelf
+                     
+                     
+copyContentCompiler :: Compiler Resource CopyFile
+copyContentCompiler = getIdentifier >>^ CopyFile . (\x -> "")
 
 main :: IO ()
 main = hakyll $ do
-    match "img/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  
+  match "content/*" $ do
+    route idRoute
+    compile copyContentCompiler
+  
+  match "img/*" $ do
+    route   idRoute
+    compile copyFileCompiler
         
-    match "js/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "js/*" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  match "css/*" $ do
+    route   idRoute
+    compile compressCssCompiler
         
-    match "doc/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "doc/*" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "templates/*" $ compile templateCompiler
+  match "templates/*" $ compile templateCompiler
 
-    match (list ["index.html", "portfolio.html", "whereiam.html", "hireme.html"]) $ do
-        route   $ setExtension "html"
-        compile $ staticPageCompiler
-            >>> applyTemplateCompiler "templates/default.html"
-            >>> relativizeUrlsCompiler
+  match (list ["index.html", "portfolio.html", "whereiam.html", "hireme.html"]) $ do
+    route   $ setExtension "html"
+    compile $ staticPageCompiler
+      >>> applyTemplateCompiler "templates/default.html"
+      >>> relativizeUrlsCompiler
