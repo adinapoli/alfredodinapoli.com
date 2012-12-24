cc2303e395cadc6d9d0f92785dcdc6c58e0bf7f7
Alfredo Di Napoli <alfredod@cakesolutions.net>
2012-12-24 11:23:25 +0100

    Site update

--- fabfile.py
+++ fabfile.py
@@ -5,12 +5,10 @@ def publish(comment="Site Update"):
     Invoke this to publish everything.
     """
     update_site()
-    #commit(comment)
+    commit(comment)
     push("master")
 
     sync_with_rsync()
-    publish_to_bitbucket()
-
 
 def sync_with_rsync():
     local("rsync -r _site/* ~/github/adinapoli.bitbucket.org/")
@@ -21,25 +19,6 @@ def update_site():
 
 def commit(comment):
     local("git add . && git commit -m \"%s\"" % comment)
-
-def create_tmp_folder():
-    local("mkdir -p ../tmp")
-
-def copy_site():
-    local("cp -r _site/* ../tmp/")
-
-def publish_to_bitbucket(comment="Site Update"):
-    with lcd("~/github/adinapoli.bitbucket.org"):
-        local("git pull")
-        commit(comment)
-        push("master")
-
-def publish_to_gh_pages(comment="Site Update"):
-    with lcd("../gh-pages"):
-        local("rm -rf *")
-        local("cp -r ../tmp/* .")
-        commit(comment)
-        push("gh-pages")
 
 def push(branch):
     local("git push origin %s" % branch)
